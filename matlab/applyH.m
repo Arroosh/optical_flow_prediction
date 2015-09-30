c7bbcf6c680551958522703c05f5d2d34e0f7642
Jacob Walker <jcwalker@cs.cmu.edu>
2015-09-30 16:37:06 -0400

    .:

--- matlab/applyH.m
+++
@@ -1,8 +0,0 @@
-function [rectI, finalMask] = applyH(img, H, mask)
-  tform = projective2d(H');
-  Rout = imref2d(size(img));
-  [rectI] =imwarp(img,tform, 'OutputView', Rout);
-  [finalMask] =imwarp(mask,tform, 'OutputView', Rout);
-
-  % imshow(rectI);
-end
